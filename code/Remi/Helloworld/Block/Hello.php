f1a0d61f02f7b76d49bea494a5a1440a62234015
DEIAS Remi <remi.deias@boticinal.com>
2020-10-05 13:18:03 +0200

    Complete Module

---
+++ code/Remi/Helloworld/Block/Hello.php
@@ -0,0 +1,5 @@
+<?php
+namespace Remi\Helloworld\Block;
+class Hello extends \Magento\Framework\View\Element\Template
+{
+}
--- code/Remi/Helloworld/Controller/Say/Hello.php
+++ code/Remi/Helloworld/Controller/Say/Hello.php
@@ -4,7 +4,8 @@ class Hello extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-       echo 'Execute Action Say_Hello OK';
-       die();
+        $this->_view->loadLayout();
+        $this->_view->getLayout()->initMessages();
+        $this->_view->renderLayout();
     }
 }

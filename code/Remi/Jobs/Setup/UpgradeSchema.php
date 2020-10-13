4609f3fc5bff938bf0ba74739b4efb5c672d6eb0
DEIAS Remi <remi.deias@boticinal.com>
2020-10-13 15:36:11 +0200

    final grid

--- code/Remi/Jobs/Setup/UpgradeSchema.php
+++ code/Remi/Jobs/Setup/UpgradeSchema.php
@@ -134,6 +134,41 @@ class UpgradeSchema implements UpgradeSchemaInterface
             // Execute SQL to create the table
             $installer->getConnection()->createTable($table);
         }
+
+        if (version_compare($context->getVersion(), '1.0.0.2') < 0) {
+ 
+            /**
+             * Add full text index to our table department
+             */
+ 
+            $tableName = $installer->getTable('remi_department');
+            $fullTextIntex = array('name'); // Column with fulltext index, you can put multiple fields
+ 
+ 
+            $setup->getConnection()->addIndex(
+                $tableName,
+                $installer->getIdxName($tableName, $fullTextIntex, \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT),
+                $fullTextIntex,
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+ 
+            /**
+             * Add full text index to our table jobs
+             */
+ 
+            $tableName = $installer->getTable('remi_job');
+            $fullTextIntex = array('title', 'type', 'location', 'description'); // Column with fulltext index, you can put multiple fields
+ 
+ 
+            $setup->getConnection()->addIndex(
+                $tableName,
+                $installer->getIdxName($tableName, $fullTextIntex, \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT),
+                $fullTextIntex,
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+            );
+ 
+        }
+
  
         $installer->endSetup();
     }
